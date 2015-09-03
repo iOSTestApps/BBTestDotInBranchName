0e0deb6d242b6efa9b3fa0b46ad8a6f38725da91
Christopher Stott <christopherstott@gmail.com>
2015-09-02 20:04:17 -0700

    initial

---
+++ BBTestDotInBranchName/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBTestDotInBranchName
+//
+//  Created by Chris on 2015-09-02.
+//  Copyright (c) 2015 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
